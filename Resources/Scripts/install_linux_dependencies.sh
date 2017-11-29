14ef89cdc8c82d506338dc2386cc6dc397502e30
aojeda <alejo.ojeda83@gmail.com>
2017-11-29 11:55:31 -0800

    Install the LSL library and other missing dependencies

--- Resources/Scripts/install_linux_dependencies.sh
+++ Resources/Scripts/install_linux_dependencies.sh
@@ -5,4 +5,7 @@ apt-get -y install build-essential
 
 # install Juce dependencies
 apt-get -y install freeglut3-dev libfreetype6-dev libxinerama-dev libxcursor-dev libasound2-dev
+apt-get -y install libhdf5-10 libhdf5-cpp-11 libhdf5-dev hdf5-helpers
+apt-get -y install libzmq1 libzmq3-dev libzmq5
 
+cp ../../Source/Plugins/LSLOutlet/liblsl64.so /usr/lib/liblsl64.so.1.0.0

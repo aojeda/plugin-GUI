2e4d7f84a1cdd2c106eebac7d4a679aee6128303
CharlesGzq <me@ziqigan.com>
2017-12-05 12:50:27 -0800

    Adding Frame2LSL plugin

---
+++ Source/Plugins/Frame2LSL/FrameGrabberEditor.cpp
@@ -0,0 +1,137 @@
+/*
+    ------------------------------------------------------------------
+
+    This file is part of the Open Ephys GUI
+    Copyright (C) 2014 Open Ephys
+
+    ------------------------------------------------------------------
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "FrameGrabberEditor.h"
+#include <stdio.h>
+
+
+FrameGrabberEditor::FrameGrabberEditor(GenericProcessor* parentNode, bool useDefaultParameterEditors=true)
+    : GenericEditor(parentNode, useDefaultParameterEditors), lastFrameCount(0)
+
+{
+  desiredWidth = 350;
+
+	FrameGrabber* proc = (FrameGrabber*) parentNode;
+
+	sourceLabel = new Label("video source", "Source");
+	sourceLabel->setBounds(10,40,90,20);
+  sourceLabel->setFont(Font("Small Text", 12, Font::plain));
+  sourceLabel->setColour(Label::textColourId, Colours::darkgrey);
+	addAndMakeVisible(sourceLabel);
+
+  sourceCombo = new ComboBox();
+  sourceCombo->setBounds(110,40,220,20);
+  sourceCombo->addListener(this);
+
+	std::vector<std::string> formats = proc->getFormats();
+  for (int i=0; i<formats.size(); i++)
+    sourceCombo->addItem(formats.at(i), i+1);
+	addAndMakeVisible(sourceCombo);
+
+	colorLabel = new Label("color mode label", "Color");
+	colorLabel->setBounds(10,75,50,20);
+  colorLabel->setFont(Font("Small Text", 12, Font::plain));
+  colorLabel->setColour(Label::textColourId, Colours::darkgrey);
+	addAndMakeVisible(colorLabel);
+
+  colorCombo = new ComboBox();
+  colorCombo->setBounds(110,75,75,20);
+  colorCombo->addListener(this);
+  colorCombo->addItem("Gray", 1);
+	colorCombo->addItem("RGB", 2);
+	colorCombo->setSelectedItemIndex(proc->getColorMode(), dontSendNotification);
+	addAndMakeVisible(colorCombo);
+
+  fpsLabel = new Label("fps label", "FPS:");
+	fpsLabel->setBounds(200, 75, 50, 20); // 200,100,50,20);
+  fpsLabel->setFont(Font("Small Text", 12, Font::plain));
+  fpsLabel->setColour(Label::textColourId, Colours::darkgrey);
+	addAndMakeVisible(fpsLabel);
+
+  refreshButton = new UtilityButton("Refresh", Font ("Small Text", 12, Font::plain));
+  refreshButton->addListener(this);
+  refreshButton->setBounds(260, 75, 70, 20);
+  addAndMakeVisible(refreshButton);
+  startTimer(1000);  // update FPS label once per second
+}
+
+
+FrameGrabberEditor::~FrameGrabberEditor()
+{
+}
+
+void FrameGrabberEditor::updateSettings()
+{
+	FrameGrabber* proc = (FrameGrabber*) getProcessor();
+	colorCombo->setSelectedItemIndex(proc->getColorMode(), dontSendNotification);
+	updateDevices();
+	int deviceIndex = proc->getCurrentFormatIndex();
+	if (deviceIndex >= 0)
+		sourceCombo->setSelectedItemIndex(deviceIndex, sendNotificationAsync);
+}
+
+void FrameGrabberEditor::comboBoxChanged(ComboBox* cb)
+{
+  if (cb == colorCombo){
+		int index = cb->getSelectedItemIndex();
+		FrameGrabber* proc = (FrameGrabber*) getProcessor();
+		proc->setColorMode(index);
+  }
+	else if (cb == sourceCombo){
+		int index = cb->getSelectedItemIndex();
+		FrameGrabber* proc = (FrameGrabber*) getProcessor();
+		if (proc->isCameraRunning())
+		  proc->stopCamera();
+    sourceCombo->setEnabled(false);
+    colorCombo->setEnabled(false);
+    refreshButton->setEnabled(false);
+		proc->startCamera(index);
+	}
+}
+
+
+void FrameGrabberEditor::buttonEvent(Button* button)
+{
+	if (button == refreshButton)
+		updateDevices();
+}
+
+
+void FrameGrabberEditor::updateDevices()
+{
+	sourceCombo->clear(dontSendNotification);
+	FrameGrabber* proc = (FrameGrabber*) getProcessor();
+	std::vector<std::string> formats = proc->getFormats();
+	for (int i=0; i<formats.size(); i++)
+	  sourceCombo->addItem(formats.at(i), i+1);
+}
+
+
+void FrameGrabberEditor::timerCallback()
+{
+	FrameGrabber* proc = (FrameGrabber*) getProcessor();
+	juce::int64 frameCount = proc->getFrameCount();
+	int fps = frameCount - lastFrameCount;
+	lastFrameCount = frameCount;
+	fpsLabel->setText(String("FPS: ") + String(fps), dontSendNotification);
+}
---
+++ Source/Plugins/Frame2LSL/README.md
@@ -0,0 +1,62 @@
+# FrameGrabber
+
+A simple video4linux2-based video frame grabber plugin for the open-ephys 
+[plugin-GUI](https://github.com/open-ephys/plugin-GUI/). It retrieves frames 
+from any device supported by [v4l2](http://linuxtv.org/downloads/v4l-dvb-apis/) 
+and displays them using [opencv](http://opencv.org/). Moreover, 
+frames can be saved to disk (in jpg format) together with open-ephys hardware 
+time stamps making it easy to synchronize frames and recorded data later on. 
+Depending on your camera this approach should be useful for most 
+(behavioral) tracking experiments. In case the experiment requires very precise 
+time stamps, e.g., high-speed whisker tracking, it might be a good idea to use a 
+camera that can send TTL pulses for each frame.
+
+Currently, the plugin only runs under Linux. However, contributions to make it 
+also run under Windows (e.g., using directshow) are highly welcome.
+
+What should work:
+
+- capturing frames from any v4l2 supported camera (using mmap; pixel formats: YUYV, MJPG)
+- saving frames in jpeg format; the file name format is "frame\_{frame\_index}\_{experiment\_number}\_{recording\_number}.jpg"
+- saving frame index, experiment number, recording number, and hardware/software time stamps to a separate csv file to make post-processing easier
+- basic controls via ui (image quality, color, recording mode, frame counter resetting, frame directory name)
+- saving/loading parameters
+
+To-do:
+
+- add further video APIs, e.g., directshow
+- python/matlab functions to read frames and timestamps (including optional interpolation)
+
+## Dependencies
+
+This plugin requires the following libraries
+
+- video4linux2 (i.e. libv4l-0 and libv4l-dev under Ubuntu and Linux Mint)
+- opencv (version 2.4.x; core, dev, and highgui packages)
+
+## Installation
+
+Copy the FrameGrabber folder to the plugin folder of your GUI. Then build 
+the all plugins as described in the [wiki](https://open-ephys.atlassian.net/wiki/display/OEW/Linux).
+
+**Important** 
+It seems that there is a data type clash between JUCE and opencv. In my case, I 
+had to replace all "int64" by "juce::int64" in 
+[CoreServices.h](https://github.com/open-ephys/plugin-GUI/blob/master/Source/CoreServices.h), 
+[CoreServices.cpp](https://github.com/open-ephys/plugin-GUI/blob/master/Source/CoreServices.cpp), 
+[GenericProcessor.h](https://github.com/open-ephys/plugin-GUI/blob/master/Source/Processors/GenericProcessor/GenericProcessor.h),
+and [GenericProcessor.cpp](https://github.com/open-ephys/plugin-GUI/blob/master/Source/Processors/GenericProcessor/GenericProcessor.cpp).
+Note that this does not affect the rest of the GUI as it is using the juce 
+namespace anyway.
+
+## Changing camera parameters
+
+The v4l2 library comes with some tools that can be used to control camera 
+parameters. The easiest way to see all available v4l2 controls is to use the 
+v4l2-ctl tool from cmdline:
+
+*v4l2-ctl --all*
+
+An alternative is to use [guvcview](http://guvcview.sourceforge.net).
+
+

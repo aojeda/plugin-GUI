144466f4d6c8541470fd297525138d1a2a00e31e
aojeda <alejo.ojeda83@gmail.com>
2018-06-04 13:20:56 -0700

    Update install dependency script

--- Resources/Scripts/install_linux_dependencies.sh
+++ Resources/Scripts/install_linux_dependencies.sh
@@ -13,4 +13,5 @@ apt-get -y install libhdf5-10 libhdf5-cpp-11 libhdf5-dev hdf5-helpers
 apt-get -y install libzmq1 libzmq3-dev libzmq5
 apt-get -y install libv4l-0 libv4l-dev
 apt-get -y install libcv2.4 libhighgui2.4 libcv-dev libopencv-dev ffmpeg
+apt-get -y install libxrandr-dev libhdf5-dev
 cp ../../Source/Plugins/LSLOutlet/liblsl64.so /usr/lib/liblsl64.so.1.0.0
